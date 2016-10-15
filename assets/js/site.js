3675b1a82272662846c8c5b2ec1581621b4c4967
Indrashish Ghosh <mail@indrashishghosh.com>
2016-10-15 14:23:53 +0530

    Added tracking code

---
+++ assets/js/site.js
@@ -0,0 +1,33 @@
+var signages = document.querySelectorAll('.listing');
+
+Array.prototype.forEach.call(signages, function(signage, i){
+
+	var downloadBtn = signage.querySelectorAll('.listing-download')[0];
+
+	var title = signage.querySelectorAll('.listing-title')[0].innerHTML;
+	var lang = signage.dataset.lang;
+
+	var langCode = null;
+	
+
+	switch(lang) {
+    case "en":
+        langCode = 1;
+        break;
+    case "hi":
+        langCode = 2;
+        break;
+    case "gu":
+        langCode = 3;
+        break;
+    default:
+        langCode = 1;
+	}
+
+	downloadBtn.onclick = function(event){
+		event.preventDefault();
+		// console.log('send', 'event', 'Signage', 'Download', title, langCode)
+		ga('send', 'event', 'Signage', 'Download', title, langCode);
+	};
+	
+});
