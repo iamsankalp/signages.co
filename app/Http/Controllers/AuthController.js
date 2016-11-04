90a5ae6c52b2bd2c0606aa997d0f32dd9f390642
Indrashish Ghosh <mail@indrashishghosh.com>
2016-11-04 17:49:45 +0530

    Temp disbale hosts matching in csrf check

--- app/Http/Controllers/AuthController.js
+++ app/Http/Controllers/AuthController.js
@@ -62,42 +62,42 @@ class AuthController {
 	}
 
 
-	* doSignup(request, response) {
+* doSignup(request, response) {
 
-		const userData = request.only('email', 'password')
+	const userData = request.only('email', 'password')
 
-    const rules = {
-      email: 'required|email|unique:users',
-      password: 'required|min:6|max:30',
-    }
+  const rules = {
+    email: 'required|email|unique:users',
+    password: 'required|min:6|max:30',
+  }
 
-		const messages = {
-		  'email.required': 'Why you no give email?',
-		  'password.min': 'Password should be atleast 6 characters long.',
-		  'password.required': 'Give passwaord maccha!'
-		}
-
-    const validation = yield Validator.validate(userData, rules, messages);
-
-    if (validation.fails()) {
-      yield request
-        .withAll()
-        .andWith({errors: validation.messages()})
-        .flash()
-      response.redirect('back')
-    }
-
-    // Validation passed, create the user.
-    const user = new User();
-
-    user.email = request.input('email');
-    user.password = yield Hash.make(request.input('password'));
-
-    yield user.save();
-    yield request.auth.login(user);
-
-    response.redirect('/');
+	const messages = {
+	  'email.required': 'Why you no give email?',
+	  'password.min': 'Password should be atleast 6 characters long.',
+	  'password.required': 'Give password maccha!'
 	}
+
+  const validation = yield Validator.validate(userData, rules, messages);
+
+  if (validation.fails()) {
+    yield request
+      .withAll()
+      .andWith({errors: validation.messages()})
+      .flash()
+    response.redirect('back')
+  }
+
+  // Validation passed, create the user.
+  const user = new User();
+
+  user.email = request.input('email');
+  user.password = yield Hash.make(request.input('password'));
+
+  yield user.save();
+  yield request.auth.login(user);
+
+  response.redirect('/');
+}
 
 
 	* doLogout(request, response) {
